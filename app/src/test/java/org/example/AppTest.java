23ecbc7fa58407f5f52e187fd41b7ce9d7e7cc47
Arun Venkatesan Rajagopalan <arunvenkatesan.87@gmail.com>
2026-08-23 00:29:43 -0400

    Day 1 - Integrate Java application with OpenAI API

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        AiQeCopilot classUnderTest = new AiQeCopilot();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
